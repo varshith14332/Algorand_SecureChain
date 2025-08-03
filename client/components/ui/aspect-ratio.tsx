29976193edf8d220462b86343a8514d460805305
varshith14332 <varshith0304@gmail.com>
2025-08-03 20:26:44 +0530

    Added the aspect-ratio logic

---
+++ client/components/ui/aspect-ratio.tsx
@@ -0,0 +1,11 @@
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+
+const AspectRatio = AspectRatioPrimitive.Root;
+
+export { AspectRatio };
+export type AspectRatioProps = AspectRatioPrimitive.AspectRatioProps;
+export type AspectRatioElement = React.ElementRef<typeof AspectRatioPrimitive.Root>;
+export type AspectRatioComponent = typeof AspectRatioPrimitive.Root;
+export type AspectRatioRootProps = React.ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>;
+export type AspectRatioRootElement = React.ElementRef<typeof AspectRatioPrimitive.Root>;
+export type AspectRatioRootComponent = typeof AspectRatioPrimitive.Root;
