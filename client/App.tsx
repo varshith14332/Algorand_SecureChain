6e081721cedaaba21791ac3847372ee88abab8ae
varshith14332 <varshith0304@gmail.com>
2025-08-03 19:46:11 +0530

    Added the app.tsx code for routing

---
+++ client/App.tsx
@@ -0,0 +1,42 @@
+import "./global.css";
+
+import { Toaster } from "@/components/ui/toaster";
+import { createRoot } from "react-dom/client";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { ScrollToTop } from "./components/ScrollToTop";
+import Index from "./pages/Index";
+import Features from "./pages/Features";
+import Developer from "./pages/Developer";
+import Security from "./pages/Security";
+import Alerts from "./pages/Alerts";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/security" element={<Security />} />
+          <Route path="/alerts" element={<Alerts />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+createRoot(document.getElementById("root")!).render(<App />);
